8bc6cc43c97e4ad556f75a89d6d55adbaf5f2fe7
whf <hfwu@arcvideo.com>
2024-03-09 15:37:18 +0800

    新增gateway网关模块

---
+++ music-gateway/src/main/java/com/whf/music/MusicGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.whf.music;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusicGatewayApplication.class, args);
+    }
+
+}
